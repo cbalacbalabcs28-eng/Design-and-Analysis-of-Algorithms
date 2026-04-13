f481623b05f1cc6afc4197338f97022951007859
cbalacbalabcs28-eng <cbalacbala_bcs28@mepcoeng.ac.in>
2026-04-13 12:07:58 +0530

    Create InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// Function to perform Insertion Sort
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements greater than key ahead
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array:\n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
